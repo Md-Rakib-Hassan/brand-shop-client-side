7f3e6520a5c9cb334f5f9665191e9de9f86e2715
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-10-18 16:24:41 +0600

    Implement testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Cards from '../components/Cards';
 import Banner from '../components/Banner';
+import Reviews from '../components/Reviews';
 
 const Home = () => {
     return (
         <div className='px-2'>
            <Banner></Banner>
             <Cards></Cards>
+            <Reviews></Reviews>
             
         </div>
     );
