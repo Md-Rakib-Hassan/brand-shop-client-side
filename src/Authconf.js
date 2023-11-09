e50fbd079b6b21f5b8bb4d6b49fe009bfe98c3d3
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-09 23:10:09 +0600

    testing the repo updating policy

--- src/Authconf.js
+++ src/Authconf.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
+//added for testing purposes//
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
